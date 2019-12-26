96fa94d46bb74e91d226ce8536e1ec55a75572a4
rewalczak <rewalczak@gmail.com>
2019-12-26 14:49:30 -0600

    Update settings.py

--- scheduler_web/settings.py
+++ scheduler_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!3&pndsvfx++3ph0$i5xey5=t-xn^+7a6eo710gwlbf2!j&+#o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
